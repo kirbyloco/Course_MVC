cd5d06b76c14dfcb2a16f0caa1d2a98d4aaaca36
耀風 <adwin82627459@gmail.com>
2021-03-23 06:01:14 +0800

    新增上課時間對照表

---
+++ Controllers/ToolController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using course_std.Models;
+
+namespace course_std.Controllers
+{
+    public class ToolController : Controller
+    {
+        private readonly ILogger<ToolController> _logger;
+
+        public ToolController(ILogger<ToolController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CourseTime()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
