5c52ec55dfce5ab8d390565b8e966aa32719da4a
耀風 <adwin82627459@gmail.com>
2021-03-23 05:18:51 +0800

    初始化dotnet MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace course_std.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
