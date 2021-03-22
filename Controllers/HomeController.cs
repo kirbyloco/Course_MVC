1f8f710e74d62ad1d3b106bf50bd2589463f0887
耀風 <adwin82627459@gmail.com>
2021-03-23 05:39:04 +0800

    修改路徑

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace course_std.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
