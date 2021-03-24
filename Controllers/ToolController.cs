f433262593d5ac5ed7b2a4f0fe7c66ee3954a165
耀風 <adwin82627459@gmail.com>
2021-03-25 05:53:56 +0800

    新增畢業學分查詢（WIP）

--- Controllers/ToolController.cs
+++ Controllers/ToolController.cs
@@ -22,6 +22,10 @@ namespace course_std.Controllers
         {
             return View();
         }
+        public IActionResult Credits()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
