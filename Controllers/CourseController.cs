07c7d526d7d1c2cc77892ec2e8365a188d80a345
耀風 <adwin82627459@gmail.com>
2021-03-26 06:21:22 +0800

    新增選課頁面（WIP）

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using course_std.Models;
+
+namespace course_std.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Addrop()
+        {
+            return View();
+        }
+        public IActionResult Result()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
