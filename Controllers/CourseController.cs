03eaa5787c5f445db6393b6b456dc5affb8fad17
耀風 <adwin82627459@gmail.com>
2021-04-08 15:27:55 +0800

    新增搜尋資料庫

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -22,13 +22,20 @@ namespace course_std.Controllers
 
         public async Task<IActionResult> Addrop(string academic, string schoolsys, string grade)
         {
-            Dictionary<string, string> data = new Dictionary<string, string>();
-            data.Add("academic", academic);
-            data.Add("schoolsys", schoolsys);
-            data.Add("grade", grade);
             dynamic model = new ExpandoObject();
-            model.Course = await _context.Course.ToListAsync();
-            model.Data = data;
+            model.Academic = academic;
+            model.Schoolsys = schoolsys;
+            model.Grade = grade;
+            var courses = from m in _context.Course select m;
+            if (!String.IsNullOrEmpty(academic) & !String.IsNullOrEmpty(schoolsys) & !String.IsNullOrEmpty(grade))
+            {
+                courses = courses.Where(s => s.Academic.Contains(academic));
+                courses = courses.Where(s => s.Schoolsys.Contains(schoolsys));
+                courses = courses.Where(s => s.Grade.Contains(grade));
+            }
+
+            model.Course = await courses.ToListAsync();
+
             return View(model);
         }
         public IActionResult Result()
