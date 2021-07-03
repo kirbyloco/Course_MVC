8337581f96ed66418bed7b96952de56d1d98b30e
耀風 <adwin82627459@gmail.com>
2021-07-03 13:51:00 +0800

    修正查詢畢業學分資料庫

--- Controllers/ToolController.cs
+++ Controllers/ToolController.cs
@@ -1,31 +1,54 @@
 ﻿using System;
+using System.Dynamic;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using course_std.Data;
 using course_std.Models;
 
 namespace course_std.Controllers
 {
     public class ToolController : Controller
     {
-        private readonly ILogger<ToolController> _logger;
+        private readonly CreditsContext _context;
 
-        public ToolController(ILogger<ToolController> logger)
+        public ToolController(CreditsContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+
+
 
         public IActionResult CourseTime()
         {
             return View();
         }
-        public IActionResult Credits(string year, string academic, string schoolsys)
+        public async Task<IActionResult> Credits(string academic, string schoolsys)
         {
             // ViewData.Add()
-            return View();
+            dynamic model = new ExpandoObject();
+            var credits = from m in _context.Credits select m;
+
+            if (!String.IsNullOrEmpty(academic) & !String.IsNullOrEmpty(schoolsys))
+            {
+                credits = credits.Where(s => s.Academic.Contains(academic));
+                credits = credits.Where(s => s.Schoolsys.Contains(schoolsys));
+            }
+            else
+            {
+                credits = credits.Where(s => s.Academic.Contains("NULL"));
+                credits = credits.Where(s => s.Schoolsys.Contains("NULL"));
+            }
+
+            model.Academic = academic;
+            model.Schoolsys = schoolsys;
+            model.Credits = await credits.ToListAsync();
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
